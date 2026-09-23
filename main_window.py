de40251a86a8f68db88aecc0aaa07586cce80fa5
r1gor-dev <r1gor@bk.ru>
2026-09-24 01:54:25 +0300

    self work checkbox features

--- main_window.py
+++ main_window.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import *
+from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -30,8 +31,10 @@ class MainWindow(QMainWindow):
 
         buttons_layout = QHBoxLayout()
         self.delete_button = QPushButton("Удалить")
+        self.delete_marked_button = QPushButton("Удалить выполненные")
         self.clear_button = QPushButton("Очистить всё")
         buttons_layout.addWidget(self.delete_button)
+        buttons_layout.insertWidget(1, self.delete_marked_button)
         buttons_layout.addWidget(self.clear_button)
         layout.addLayout(buttons_layout)
         self.counter_label = QLabel("Количество задач: 0")
@@ -43,11 +46,29 @@ class MainWindow(QMainWindow):
         self.add_button.clicked.connect(self.add_task)
         self.task_input.returnPressed.connect(self.add_task)
         self.delete_button.clicked.connect(self.delete_task)
+        self.delete_marked_button.clicked.connect(self.delete_marked_tasks)
+        self.task_list.itemChanged.connect(self.update_counter)
         self.clear_button.clicked.connect(self.clear_tasks)
 
     def update_counter(self):
         cnt = self.task_list.count()
+        marked = sum(
+            1 for i in range(cnt)
+                if self.task_list.item(i).checkState() == Qt.CheckState.Checked
+        )
         self.counter_label.setText(f"Количество задач: {cnt}")
+
+    def delete_marked_tasks(self):
+        rm_flag = False
+        for i in range(self.task_list.count()-1,-1,-1):
+            if self.task_list.item(i).checkState()==Qt.CheckState.Checked:
+                self.task_list.takeItem(i)
+                rm_flag=True
+        if rm_flag:
+            self.update_counter()
+        else:
+            QMessageBox.information(self,"Инфа",
+                "Вы ещё не выполнили задачи, чтобы их удалять.")
         
     def clear_tasks(self):
         if self.task_list.count()==0:
@@ -73,7 +94,11 @@ class MainWindow(QMainWindow):
     def add_task(self):
         text = self.task_input.text().strip()
         if text:
-            self.task_list.addItem(text)
+            item = QListWidgetItem(text)
+            item.setFlags(item.flags() | Qt.ItemFlag.ItemIsUserCheckable)
+            item.setCheckState(Qt.CheckState.Unchecked)
+            self.task_list.addItem(item)
+            # self.task_list.addItem(text)
             self.task_input.clear()
             self.update_counter()
         else:
